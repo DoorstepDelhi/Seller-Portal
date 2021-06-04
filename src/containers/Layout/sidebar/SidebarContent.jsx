a85433f7cefe36d4e2e8677b5d753ee533fb425e
ayushi212001 <71006648+ayushi212001@users.noreply.github.com>
2021-06-04 19:40:22 +0530

    Update SidebarContent.jsx

--- src/containers/Layout/sidebar/SidebarContent.jsx
+++ src/containers/Layout/sidebar/SidebarContent.jsx
@@ -10,14 +10,44 @@ const SidebarContent = ({ onClick }) => {
 
   return (
     <div className="sidebar__content">
+    <ul className="sidebar__block">
+        <SidebarLink title="Dashboard" icon="home" route="/log_in" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Products" icon="shirt" route="/products" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarCategory title="Orders" icon="cart">
+          <SidebarLink title="New" route="/pages/one" onClick={handleHideSidebar} />
+          <SidebarLink title="History" route="/pages/two" onClick={handleHideSidebar} />
+        </SidebarCategory>
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Transactions" icon="rupee" route="/Transactions" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Return" icon="cart" route="/Returns" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Support" icon="phone" route="/support" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Terms and conditions" icon="text-align-justify" route="/terms&conditions" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Privacy & Policy" icon="text-align-justify" route="/privacy&policy" onClick={handleHideSidebar} />
+    </ul>
+    <ul className="sidebar__block">
+        <SidebarLink title="Profile" icon="user" route="/profile" onClick={handleHideSidebar} />
+    </ul>
       <ul className="sidebar__block">
         <SidebarLink title="Log Out" icon="exit" route="/log_in" onClick={handleHideSidebar} />
       </ul>
       <ul className="sidebar__block">
-        <SidebarCategory title="Example Pages" icon="diamond">
-          <SidebarLink title="Page one" route="/pages/one" onClick={handleHideSidebar} />
-          <SidebarLink title="Page two" route="/pages/two" onClick={handleHideSidebar} />
-        </SidebarCategory>
+        <SidebarLink title="FAQ" icon="menu" route="/Faq" onClick={handleHideSidebar} />
+      </ul>
+      <ul className="sidebar__block">
+        <SidebarLink title="How to Use" icon="" route="/how to use" onClick={handleHideSidebar} />
       </ul>
     </div>
   );
