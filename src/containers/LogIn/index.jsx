b07f3b09bf40224bf6026def3231f3a8d1d72ddb
ayushi212001 <71006648+ayushi212001@users.noreply.github.com>
2021-06-14 13:13:16 +0530

    login updated

--- src/containers/LogIn/index.jsx
+++ src/containers/LogIn/index.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import FacebookIcon from 'mdi-react/FacebookIcon';
-import GooglePlusIcon from 'mdi-react/GooglePlusIcon';
 import LogInForm from './components/LogInForm';
 
 const LogIn = () => (
@@ -9,31 +6,11 @@ const LogIn = () => (
     <div className="account__wrapper">
       <div className="account__card">
         <div className="account__head">
-          <h3 className="account__title">Welcome to
-            <span className="account__logo"> Easy
-              <span className="account__logo-accent">DEV</span>
-            </span>
+          <h3 className="account__title">Welcome to DoorStep Delhi
           </h3>
-          <h4 className="account__subhead subhead">Start your business easily</h4>
+          <h4 className="account__subhead subhead">Seller Portal</h4>
         </div>
         <LogInForm />
-        <div className="account__or">
-          <p>Or Easily Using</p>
-        </div>
-        <div className="account__social">
-          <Link
-            className="account__social-btn account__social-btn--facebook"
-            to="/pages/one"
-          >
-            <FacebookIcon />
-          </Link>
-          <Link
-            className="account__social-btn account__social-btn--google"
-            to="/pages/one"
-          >
-            <GooglePlusIcon />
-          </Link>
-        </div>
       </div>
     </div>
   </div>
@@ -41,5 +18,3 @@ const LogIn = () => (
 
 export default LogIn;
 
-// if you want to add select, date-picker and time-picker in your app you need to uncomment the first
-// four lines in /scss/components/form.scss to add styles
